39fb659b7256f13f81e8b2a7001b6e6c57035237
s14321k <silvershinesarath@gmail.com>
2025-05-01 00:22:58 +0530

    renamed the proj. Completed Accounts

---
+++ aacounts/src/test/java/com/aacounts/Aacounts.java
@@ -0,0 +1,13 @@
+package com.aacounts;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Aacounts {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
